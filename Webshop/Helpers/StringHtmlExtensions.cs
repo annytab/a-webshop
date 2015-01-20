bff7e506828f5f89cb4ddcb590be5ab94701aa56
Fredrik Stigsson <info@annytab.se>
2015-01-20 14:25:20 +0100

    Fixed html truncation bug

--- Webshop/Helpers/StringHtmlExtensions.cs
+++ Webshop/Helpers/StringHtmlExtensions.cs
@@ -3,7 +3,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 
 namespace System
-{ 
+{
     /// <summary>
     /// This class is a helper for html and string manipulations
     /// Author: Rob Volk, https://github.com/robvolk/
@@ -12,71 +12,88 @@ namespace System
     public static class StringHtmlExtensions
     {
         /// <summary>
-        /// Truncates a string containing HTML to a number of text characters, keeping whole words.
+        /// Truncates a string containing HTML to a number of text characters, keeping whole words
         /// The result contains HTML and any tags left open are closed.
         /// </summary>
         public static string TruncateHtml(this string html, int maxCharacters, string trailingText)
         {
-            if (string.IsNullOrEmpty(html))
+            // Return if the string is null or empty
+            if (string.IsNullOrEmpty(html) == true)
+            {
                 return html;
+            }
 
-            // find the spot to truncate
-            // count the text characters and ignore tags
-            var textCount = 0;
-            var charCount = 0;
-            var ignore = false;
+            // Count the characters and ignore tags
+            Int32 textCount = 0;
+            Int32 charCount = 0;
+            bool ignore = false;
+
+            // Loop all the characters in the html string
             foreach (char c in html)
             {
+                // Increase the character count
                 charCount++;
+
+                // Count characters that are outside of html tags
                 if (c == '<')
+                {
                     ignore = true;
-                else if (!ignore)
+                }
+                else if (ignore == false)
+                {
                     textCount++;
+                }
 
+                // Reset the ignore boolean
                 if (c == '>')
+                {
                     ignore = false;
+                }
 
-                // stop once we hit the limit
+                // Stop once we hit the limit
                 if (textCount >= maxCharacters)
+                {
                     break;
+                }
             }
 
             // Truncate the html and keep whole words only
-            var trunc = new StringBuilder(html.TruncateWords(charCount));
+            StringBuilder trunc = new StringBuilder(html.TruncateWords(charCount));
 
-            // keep track of open tags and close any tags left open
-            var tags = new Stack<string>();
-            var matches = Regex.Matches(trunc.ToString(),
-                @"<((?<tag>[^\s/>]+)|/(?<closeTag>[^\s>]+)).*?(?<selfClose>/)?\s*>",
+            // Keep track of open tags and close any tags left open
+            Stack<string> tags = new Stack<string>();
+            MatchCollection matches = Regex.Matches(trunc.ToString(), @"<((?<tag>[^\s/>]+)|/(?<closeTag>[^\s>]+)).*?(?<selfClose>/)?\s*>",
                 RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Multiline);
 
+            // Loop all the matches
             foreach (Match match in matches)
             {
                 if (match.Success)
                 {
-                    var tag = match.Groups["tag"].Value;
-                    var closeTag = match.Groups["closeTag"].Value;
+                    string tag = match.Groups["tag"].Value;
+                    string closeTag = match.Groups["closeTag"].Value;
 
-                    // push to stack if open tag and ignore it if it is self-closing, i.e. <br />
-                    if (!string.IsNullOrEmpty(tag) && string.IsNullOrEmpty(match.Groups["selfClose"].Value))
-                        tags.Push(tag);
-
-                    // pop from stack if close tag
-                    else if (!string.IsNullOrEmpty(closeTag))
+                    // Push to stack if open tag and ignore it if it is self-closing, i.e. <br />
+                    if (string.IsNullOrEmpty(tag) == false && string.IsNullOrEmpty(match.Groups["selfClose"].Value) == true)
                     {
-                        // pop the tag to close it.. find the matching opening tag
+                        tags.Push(tag);
+                    }
+                    else if (string.IsNullOrEmpty(closeTag) == false)
+                    {
+                        // Pop the tag to close it.. find the matching opening tag
                         // ignore any unclosed tags
-                        while (tags.Pop() != closeTag && tags.Count > 0)
-                        { }
+                        while (tags.Count > 0 && tags.Pop() != closeTag) { }
                     }
                 }
             }
 
+            // Add the trailing text
             if (html.Length > charCount)
-                // add the trailing text
+            {
                 trunc.Append(trailingText);
+            }
 
-            // pop the rest off the stack to close remainder of tags
+            // Pop the rest off the stack to close remainder of tags
             while (tags.Count > 0)
             {
                 trunc.Append("</");
@@ -84,6 +101,7 @@ namespace System
                 trunc.Append('>');
             }
 
+            // Return the html string
             return trunc.ToString();
 
         } // End of the TruncateHtml method
@@ -97,6 +115,22 @@ namespace System
             return html.TruncateHtml(maxCharacters, null);
 
         } // End of the TruncateHtml method
+
+        /// <summary>
+        /// Truncates a string containing HTML to the first occurrence of a delimiter
+        /// </summary>
+        /// <param name="html">The HTML string to truncate</param>
+        /// <param name="delimiter">The delimiter</param>
+        /// <param name="comparison">The delimiter comparison type</param>
+        public static string TruncateHtmlByDelimiter(this string html, string delimiter, StringComparison comparison = StringComparison.Ordinal)
+        {
+            var index = html.IndexOf(delimiter, comparison);
+            if (index <= 0) return html;
+
+            var r = html.Substring(0, index);
+            return r.TruncateHtml(r.StripHtml().Length);
+
+        } // End of the TruncateHtmlByDelimiter
 
         /// <summary>
         /// Strips all HTML tags from a string
@@ -131,7 +165,6 @@ namespace System
 
         } // End of the Truncate method
 
-
         /// <summary>
         /// Truncates text and discars any partial words left at the end
         /// </summary>
@@ -142,7 +175,7 @@ namespace System
         } // End of the TruncateWords method
 
         /// <summary>
-        /// Truncates text and discars any partial words left at the end
+        /// Truncates text and discard any partial words left at the end
         /// </summary>
         public static string TruncateWords(this string text, int maxCharacters, string trailingText)
         {
@@ -151,7 +184,7 @@ namespace System
 
             // trunctate the text, then remove the partial word at the end
             return Regex.Replace(text.Truncate(maxCharacters),
-                @"\s+[^\s]+$", string.Empty, RegexOptions.IgnoreCase | RegexOptions.Compiled) + trailingText;
+                @"\s+[^\s>]+$", string.Empty, RegexOptions.IgnoreCase | RegexOptions.Compiled) + trailingText;
 
         } // End of the TruncateWords method
 
